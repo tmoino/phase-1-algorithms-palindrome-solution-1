20dae580e28952bd5f7894513163ed3509fdaf89
tmoino <tony.moino@student.moringaschool.com>
2022-06-16 14:28:09 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+// reversing the string 
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+// checking whether the string which is reversed is same as the original string or not
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) return true
+  
+  return false
+}  
+
+  isPalindrome('abba');
+  isPalindrome('racecar');
+  isPalindrome ('a');
+  isPalindrome('robot');
+  isPalindrome('ab');
+  isPalindrome ('mom');
 
 /* 
   Add your pseudocode here
+
+  if the string is === to the reversed one then it will 
+  return true 
+  else 
+  return false
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +43,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+
 }
 
 module.exports = isPalindrome;
